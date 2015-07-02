61c07f52a6b9e4a1cc52c6710eded3cd5308c48a
kfengc27 <kfengc27>
2015-07-02 20:59:07 +0800

    开始Part2前的备份

---
+++ DNApp/LearnViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LearnViewController.swift
+//  DNApp
+//
+//  Created by kfengc27 on 15/7/2.
+//  Copyright (c) 2015年 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    @IBOutlet weak var dialogView: DesignableView!
+    
+    @IBOutlet weak var bookImageView: SpringImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        dialogView.animate()
+        
+    }
+    
+    
+    @IBAction func learnButtonDidTouch(sender: AnyObject) {
+        bookImageView.animation="pop"
+        bookImageView.animate()
+
+    }
+    
+    @IBAction func closeButtonDidTouch(sender: AnyObject) {
+        
+        dialogView.animation="fall"
+        dialogView.animateNext{
+            self.dismissViewControllerAnimated(true, completion: nil)
+        
+        
+        }
+    }
+    
+   }
